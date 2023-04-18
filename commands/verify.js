afb2ea433bc845e85ccf40bb425c05f1c2702be6
alec <31748545+burturt@users.noreply.github.com>
2023-04-17 18:59:22 -0700

    finish final role giving system

--- commands/verify.js
+++ commands/verify.js
@@ -10,14 +10,13 @@ const {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
-  PermissionFlagsBits,
 } = require("discord.js");
 const db = require("../database");
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("verify")
     .setDescription("Verify your school affiliation")
-    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
     .addSubcommand((subcommand) =>
       subcommand.setName("ucla").setDescription("Verify as a ucla student")
     ),
@@ -64,24 +63,25 @@ module.exports = {
 
       await interaction.followUp({
         content:
-          "Submit a verification request by clicking the first button, and then click the second button after you have submitted.\n\nThis verification request will expire in 15 minutes.",
+          'Submit a verification request using the link below, and then click the "check verification" button after you have submitted.\n\nThis verification request will expire in 15 minutes.',
         components: [row],
       });
     }
   },
   async onMessageInteraction(interaction) {
     if (interaction.customId.startsWith("verify--ucla--")) {
+      await interaction.deferReply({ ephemeral: true });
       const uuid = interaction.customId.substring("verify--ucla--".length);
-      const verifyRequest = db.verifyRequests.findOne({
+      const verifyRequest = await db.verifyRequests.findOne({
         where: {
           discordID: interaction.user.id,
           verifyToken: uuid,
-          expiration: { [Op.lt]: new Date() },
+          expiration: { [Op.gt]: Date.now() },
         },
       });
       console.log(uuid);
       if (verifyRequest === null) {
-        await interaction.reply(
+        await interaction.followUp(
           "No active verification request was found for your discord account. Re-run the /verify command again."
         );
       } else {
@@ -101,7 +101,6 @@ module.exports = {
                   1000
             ).toISOString(),
         });
-        console.log(JSON.stringify(res.data));
         const match = res.data.responses.find((r) => {
           return (
             r.answers[config["verification"]["discord id field id"]].textAnswers
@@ -120,7 +119,32 @@ module.exports = {
           return;
         }
         console.log("MATCH: " + JSON.stringify(match));
-        return res.data.responses[0].answers;
+        await verifyRequest.destroy();
+        const UCLARole = await interaction.guild.roles.cache.find(
+          (r) => r.name === "UCLA"
+        );
+        // Check if email was already used, and if so, remove role from old user if they exist
+        const oldRecord = await db.verifiedUsers.findByPk(
+          match.respondentEmail
+        );
+        if (oldRecord !== null) {
+          const oldUser = await interaction.guild.members.fetch(
+            await oldRecord.discordID
+          );
+          try {
+            await oldUser.roles.remove(UCLARole);
+          } catch {
+            // Don't crash if can't find old user
+          }
+          await oldRecord.destroy();
+        }
+        await db.verifiedUsers.create({
+          discordID: interaction.user.id,
+          email: match.respondentEmail,
+          university: "UCLA",
+        });
+        await interaction.member.roles.add(UCLARole);
+        await interaction.followUp("You have been verified as a UCLA student!");
       }
     } else {
       await interaction.reply({

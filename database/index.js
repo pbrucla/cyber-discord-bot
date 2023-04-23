595678af2633312e147a23060bc25476d5a95545
alec <31748545+burturt@users.noreply.github.com>
2023-04-23 16:33:17 -0700

    disable sequelize logging

--- database/index.js
+++ database/index.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "data/database.sqlite",
+  logging: false,
 });
 
 let reminders = sequelize.define("reminders", {

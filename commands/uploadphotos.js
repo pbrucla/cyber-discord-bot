c383087065d38d4ace19f15b67d363acd13ba8c3
ktm19 <katiemin24@gmail.com>
2025-09-11 19:23:40 -0700

    make pull request fixes/improvements

--- commands/uploadphotos.js
+++ commands/uploadphotos.js
@@ -3,7 +3,7 @@ import { authorize } from "../utilities/google-auth.js"
 import fs from "fs"
 import { ContextMenuCommandBuilder, ApplicationCommandType } from "discord.js"
 import axios from "axios"
-import path from "path"
+import stream from "stream"
 
 export const data = new ContextMenuCommandBuilder().setName("upload photos").setType(ApplicationCommandType.Message)
 
@@ -12,46 +12,67 @@ export async function execute(interaction) {
   const message = await interaction.channel.messages.fetch(interaction.targetId)
   await interaction.deferReply() // Discord requires an acknowledgement within 3 seconds. this allows the response to be deferred, with an "<application> is thinking..." response in the meantime
 
-  let auth = await authorize()
-  const drive = google.drive({ version: "v3", auth })
-  const folderId = "[FOLDER ID]" // replace this with the id of the desired folder (the part of the url after "folders/")
-  const downloadDir = path.join(import.meta.dirname, "..", "downloads")
-  let count = 0
-
-  for (const [key, value] of message["attachments"]) {
-    if (value["contentType"].substring(0, 5) !== "image") continue
-    /* credit to https://github.com/ZacTimTam/Upload-To-GDrive-Discord-Bot/tree/main for the WriteStream sections */
-    try {
-      const filePath = path.join(downloadDir, value["name"])
-      const response = await axios.get(value["url"], { responseType: "stream" })
-      const writer = fs.createWriteStream(filePath)
-      response.data.pipe(writer)
-      await new Promise((resolve, reject) => {
-        writer.on("finish", resolve)
-        writer.on("error", reject)
-      })
-
-      const res = await drive.files.create({
-        requestBody: {
-          name: value["title"],
-          mimeType: value["contentType"],
-          parents: [folderId],
-        },
-        media: {
-          mimeType: value["contentType"],
-          body: fs.createReadStream(filePath),
-        },
-      })
-      // console.log(res.data);
-      await fs.promises.unlink(filePath)
-      count++
-    } catch (error) {
-      console.error(`Failed to process file ${value["name"]}:`, error)
-    }
-  }
-  if (count === 1) {
-    interaction.editReply(`1 photo uploaded!`)
+  if (!interaction.member.roles.cache.some(role => role.name === "officer")) {
+    interaction.editReply("Insufficient permissions.")
+  } else if (!process.env.FOLDER_ID) {
+    interaction.editReply("No folder id found. Please specify it in .env.")
   } else {
-    interaction.editReply(`${count} photos uploaded!`)
+    let auth = await authorize()
+    const drive = google.drive({ version: "v3", auth })
+    let count = 0
+    let failedCount = 0
+    let failedFiles = []
+    let editedReply = ""
+
+    for (const [key, value] of message["attachments"]) {
+      if (!value["contentType"] || !value["contentType"].startsWith("image/")) continue // make sure to only upload images
+      try {
+        const response = await axios.get(value["url"], { responseType: "arraybuffer" })
+        const buffer = Buffer.from(response.data)
+        const bufferStream = new stream.PassThrough()
+        bufferStream.end(buffer)
+        const fileMetadata = {
+          name: value["name"],
+          parents: [process.env.FOLDER_ID], // folder id
+        }
+        const media = {
+          mimeType: value["contentType"],
+          body: bufferStream,
+        }
+        // upload the photo
+        const res = await drive.files.create({
+          resource: fileMetadata,
+          media,
+          fields: "id",
+        })
+        count++
+      } catch (error) {
+        failedCount++
+        failedFiles.push(`(name: ${value["name"]}, id: ${value["id"]})`)
+        console.error(`Error uploading file with id ${value["id"]}:`, error)
+        if (error["code"] === 404)
+          editedReply =
+            "404 error; please make sure the folder's id has been specified correctly and the bot has Editor access to the folder.\n"
+        else if (error["code"] === 403)
+          editedReply = "403 error; please make sure the bot has Editor access to the folder.\n"
+      }
+    }
+
+    // update the message with the results and any failures
+    if (count === 1) {
+      editedReply += "1 photo uploaded! "
+    } else {
+      editedReply += `${count} photos uploaded! `
+    }
+    if (failedCount === 1) {
+      editedReply += "1 photo was not uploaded: " + failedFiles[0]
+    } else if (failedCount > 0) {
+      editedReply += `${failedCount} photos were not uploaded: `
+      for (let i = 0; i < failedFiles.length - 1; i++) {
+        editedReply += failedFiles[i] + ", "
+      }
+      editedReply += failedFiles[failedFiles.length - 1]
+    }
+    interaction.editReply(editedReply)
   }
 }
